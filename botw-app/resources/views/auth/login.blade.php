e1a3c8d84799416110fcc4f672cbef3dbbd40fa2
Reza Ananda <73782101+dragonshck@users.noreply.github.com>
2023-01-12 19:20:25 +0700

    starter

---
+++ botw-app/resources/views/auth/login.blade.php
@@ -0,0 +1,147 @@
+
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <!-- ===============================================-->
+    <!--    Document Title-->
+    <!-- ===============================================-->
+    <title>PPA Kalvari 868 | Portal</title>
+
+
+    <!-- ===============================================-->
+    <!--    Favicons-->
+    <!-- ===============================================-->
+    <link rel="apple-touch-icon" sizes="180x180" href="../../../assets/img/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../../../assets/img/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../../../assets/img/favicons/favicon-16x16.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../../../assets/img/favicons/favicon.ico">
+    <link rel="manifest" href="{{ asset('falcon/public/assets/img/favicons/manifest.json') }}">
+    <meta name="msapplication-TileImage" content="../../../assets/img/favicons/mstile-150x150.png">
+    <meta name="theme-color" content="#ffffff">
+    <script src="{{ asset('falcon/public/assets/js/config.js') }}"></script>
+    <script src="{{ asset('falcon/public/vendors/overlayscrollbars/OverlayScrollbars.min.js') }}"></script>
+
+
+    <!-- ===============================================-->
+    <!--    Stylesheets-->
+    <!-- ===============================================-->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700%7cPoppins:300,400,500,600,700,800,900&amp;display=swap" rel="stylesheet">
+    <link href="{{ asset('falcon/public/vendors/overlayscrollbars/OverlayScrollbars.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('falcon/public/assets/css/theme-rtl.min.css') }}" rel="stylesheet" id="style-rtl">
+    <link href="{{ asset('falcon/public/assets/css/theme.min.css') }}" rel="stylesheet" id="style-default">
+    <link href="{{ asset('falcon/public/assets/css/user-rtl.min.css') }}" rel="stylesheet" id="user-style-rtl">
+    <link href="{{ asset('falcon/public/assets/css/user.min.css') }}" rel="stylesheet" id="user-style-default">
+    <script>
+      var isRTL = JSON.parse(localStorage.getItem('isRTL'));
+      if (isRTL) {
+        var linkDefault = document.getElementById('style-default');
+        var userLinkDefault = document.getElementById('user-style-default');
+        linkDefault.setAttribute('disabled', true);
+        userLinkDefault.setAttribute('disabled', true);
+        document.querySelector('html').setAttribute('dir', 'rtl');
+      } else {
+        var linkRTL = document.getElementById('style-rtl');
+        var userLinkRTL = document.getElementById('user-style-rtl');
+        linkRTL.setAttribute('disabled', true);
+        userLinkRTL.setAttribute('disabled', true);
+      }
+    </script>
+  </head>
+
+
+  <body>
+
+    <!-- ===============================================-->
+    <!--    Main Content-->
+    <!-- ===============================================-->
+    <main class="main" id="top">
+      <div class="container-fluid">
+        <div class="row min-vh-100 flex-center g-0">
+          <div class="col-lg-8 col-xxl-5 py-3 position-relative"><img class="bg-auth-circle-shape" src="{{ asset('falcon/public/assets/img/icons/spot-illustrations/bg-shape.png') }}" alt="" width="250">
+            <img class="bg-auth-circle-shape-2" src="{{ asset('falcon/public/assets/img/icons/spot-illustrations/shape-1.png') }}" alt="" width="150">
+            <div class="card overflow-hidden z-index-1">
+              <div class="card-body p-0">
+                <div class="row g-0 h-100">
+                  <div class="col-md-5 text-center bg-card-gradient">
+                    <div class="position-relative p-4 pt-md-5 pb-md-7 light">
+                      <div class="bg-holder bg-auth-card-shape" style="background-image:url({{ asset('falcon/public/assets/img/icons/spot-illustrations/half-circle.png') }});">
+                      </div>
+                      <!--/.bg-holder-->
+
+                      <div class="z-index-1 position-relative"><a class="link-light mb-4 font-sans-serif fs-4 d-inline-block fw-bolder" href="../../../index.html">Kalvari 868</a>
+                        <p class="opacity-75 text-white">Welcome!</p>
+                      </div>
+                    </div>
+                    <div class="mt-3 mb-4 mt-md-4 mb-md-5 light">
+                      {{-- <p class="text-white">Don't have an account?<br><a class="text-decoration-underline link-light" href="../../../pages/authentication/card/register.html">Get started!</a></p> --}}
+                    </div>
+                  </div>
+                  <div class="col-md-7 d-flex flex-center">
+                    <div class="p-4 p-md-5 flex-grow-1">
+                      <div class="row flex-between-center">
+                        <div class="col-auto">
+                          <h3>Account Login</h3>
+                        </div>
+                      </div>
+                        <form action="/login" method="POST">
+                        @csrf
+                        <div class="mb-3">
+                          <label class="form-label" for="card-email">Email</label>
+                          <input class="form-control" id="card-email" name="email" type="text"/>
+                        </div>
+                        <div class="mb-3">
+                          <div class="d-flex justify-content-between">
+                            <label class="form-label" for="card-password">Password</label>
+                          </div>
+                          <input class="form-control" id="card-password" name="password" type="password"/>
+                        </div>
+                        <div class="row flex-between-center">
+                          <div class="col-auto">
+                            {{-- <div class="form-check mb-0">
+                              <input class="form-check-input" type="checkbox" id="card-checkbox" checked="checked" />
+                              <label class="form-check-label mb-0" for="card-checkbox">Remember me</label>
+                            </div> --}}
+                          </div>
+                          <div class="col-auto">
+                            {{-- <a class="fs--1" href="../../../pages/authentication/card/forgot-password.html">Forgot Password?</a></div> --}}
+                        </div>
+                        <div class="mb-3">
+                          <button class="btn btn-primary d-block w-100 mt-3" type="submit" name="submit">Log in</button>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+    <!-- ===============================================-->
+    <!--    End of Main Content-->
+    <!-- ===============================================-->
+
+    <!-- ===============================================-->
+    <!--    JavaScripts-->
+    <!-- ===============================================-->
+    <script src="{{ asset('falcon/public/vendors/popper/popper.min.js') }}"></script>
+    <script src="{{ asset('falcon/public/vendors/bootstrap/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('falcon/public/vendors/anchorjs/anchor.min.js') }}"></script>
+    <script src="{{ asset('falcon/public/vendors/is/is.min.js') }}"></script>
+    <script src="{{ asset('falcon/public/vendors/fontawesome/all.min.js') }}"></script>
+    <script src="{{ asset('falcon/public/vendors/lodash/lodash.min.js') }}"></script>
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
+    <script src="{{ asset('falcon/public/vendors/list.js/list.min.js') }}"></script>
+    <script src="{{ asset('falcon/public/assets/js/theme.js') }}"></script>
+
+  </body>
+
+</html>
---
+++ botw-app/resources/views/layouts/main.blade.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <!-- ===============================================-->
+    <!--    Document Title-->
+    <!-- ===============================================-->
+    <title>PPA Kalvari 868 | XIV</title>
+
+
+    <!-- ===============================================-->
+    <!--    Favicons-->
+    <!-- ===============================================-->
+    <link rel="apple-touch-icon" sizes="180x180" href="../assets/img/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../assets/img/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/img/favicons/favicon-16x16.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../assets/img/favicons/favicon.ico">
+    <link rel="manifest" href="{{ asset('falcon-style/public/assets/img/favicons/manifest.json') }}">
+    <meta name="msapplication-TileImage" content="../assets/img/favicons/mstile-150x150.png">
+    <meta name="theme-color" content="#ffffff">
+    <script src="{{ asset('falcon-style/public/assets/js/config.js') }}"></script>
+    <script src="{{ asset('falcon-style/public/vendors/overlayscrollbars/OverlayScrollbars.min.js') }}"></script>
+
+    <!-- ===============================================-->
+    <!--    Stylesheets-->
+    <!-- ===============================================-->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700%7cPoppins:300,400,500,600,700,800,900&amp;display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700%7cPoppins:300,400,500,600,700,800,900&amp;display=swap" rel="stylesheet">
+    <link href="{{ asset('falcon/public/vendors/overlayscrollbars/OverlayScrollbars.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('falcon/public/assets/css/theme-rtl.min.css') }}" rel="stylesheet" id="style-rtl">
+    <link href="{{ asset('falcon/public/assets/css/theme.min.css') }}" rel="stylesheet" id="style-default">
+    <link href="{{ asset('falcon/public/assets/css/user-rtl.min.css') }}" rel="stylesheet" id="user-style-rtl">
+    <link href="{{ asset('falcon/public/assets/css/user.min.css') }}" rel="stylesheet" id="user-style-default">
+    <script>
+      var isRTL = JSON.parse(localStorage.getItem('isRTL'));
+      if (isRTL) {
+        var linkDefault = document.getElementById('style-default');
+        var userLinkDefault = document.getElementById('user-style-default');
+        linkDefault.setAttribute('disabled', true);
+        userLinkDefault.setAttribute('disabled', true);
+        document.querySelector('html').setAttribute('dir', 'rtl');
+      } else {
+        var linkRTL = document.getElementById('style-rtl');
+        var userLinkRTL = document.getElementById('user-style-rtl');
+        linkRTL.setAttribute('disabled', true);
+        userLinkRTL.setAttribute('disabled', true);
+      }
+    </script>
+  </head>
+
+
+  <body>
+
+    <!-- ===============================================-->
+    <!--    Main Content-->
+    <!-- ===============================================-->
+    <main class="main" id="top">
+      <div class="container" data-layout="container">
+        <script>
+          var isFluid = JSON.parse(localStorage.getItem('isFluid'));
+          if (isFluid) {
+            var container = document.querySelector('[data-layout]');
+            container.classList.remove('container');
+            container.classList.add('container-fluid');
+          }
+        </script>
+        <nav class="navbar navbar-light navbar-vertical navbar-expand-xl">
+          <script>
+            var navbarStyle = localStorage.getItem("navbarStyle");
+            if (navbarStyle && navbarStyle !== 'transparent') {
+              document.querySelector('.navbar-vertical').classList.add(`navbar-${navbarStyle}`);
+            }
+          </script>
+          <div class="d-flex align-items-center">
+            
+            <div class="toggle-icon-wrapper">
+
+              <button class="btn navbar-toggler-humburger-icon navbar-vertical-toggle" data-bs-toggle="tooltip" data-bs-placement="left" title="Toggle Navigation"><span class="navbar-toggle-icon"><span class="toggle-line"></span></span></button>
+
+            </div><a class="navbar-brand" href="/portal">
+              <div class="d-flex align-items-center py-3"><img class="me-2" src="{{ asset('assets/dawn.png') }}" alt="" width="40" /><span class="font-sans-serif">Kalvari</span>
+              </div>
+            </a>
+          </div>
+          @include('layouts.sidebar')
+          
+        </nav>
+        <div class="content">
+
+            @include('layouts.navbar')
+
+          {{-- Taruh Yield Disini --}}
+
+
+          <footer class="footer">
+            <div class="row g-0 justify-content-between fs--1 mt-4 mb-3">
+              <div class="col-12 col-sm-auto text-center">
+                <p class="mb-0 text-600">Sistem Informasi<span class="d-none d-sm-inline-block">| </span><br class="d-sm-none" /> 2023 &copy; Universitas Ma Chung</p>
+              </div>
+              <div class="col-12 col-sm-auto text-center">
+                <p class="mb-0 text-600">v1.4.0</p>
+              </div>
+            </div>
+          </footer>
+        </div>
+        <div class="modal fade" id="authentication-modal" tabindex="-1" role="dialog" aria-labelledby="authentication-modal-label" aria-hidden="true">
+          <div class="modal-dialog mt-6" role="document">
+            <div class="modal-content border-0">
+              <div class="modal-header px-5 position-relative modal-shape-header bg-shape">
+                <div class="position-relative z-index-1 light">
+                  <h4 class="mb-0 text-white" id="authentication-modal-label">Register</h4>
+                  <p class="fs--1 mb-0 text-white">Please create your free Falcon account</p>
+                </div>
+                <button class="btn-close btn-close-white position-absolute top-0 end-0 mt-2 me-2" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body py-4 px-5">
+                <form>
+                  <div class="mb-3">
+                    <label class="form-label" for="modal-auth-name">Name</label>
+                    <input class="form-control" type="text" autocomplete="on" id="modal-auth-name" />
+                  </div>
+                  <div class="mb-3">
+                    <label class="form-label" for="modal-auth-email">Email address</label>
+                    <input class="form-control" type="email" autocomplete="on" id="modal-auth-email" />
+                  </div>
+                  <div class="row gx-2">
+                    <div class="mb-3 col-sm-6">
+                      <label class="form-label" for="modal-auth-password">Password</label>
+                      <input class="form-control" type="password" autocomplete="on" id="modal-auth-password" />
+                    </div>
+                    <div class="mb-3 col-sm-6">
+                      <label class="form-label" for="modal-auth-confirm-password">Confirm Password</label>
+                      <input class="form-control" type="password" autocomplete="on" id="modal-auth-confirm-password" />
+                    </div>
+                  </div>
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="modal-auth-register-checkbox" />
+                    <label class="form-label" for="modal-auth-register-checkbox">I accept the <a href="#!">terms </a>and <a href="#!">privacy policy</a></label>
+                  </div>
+                  <div class="mb-3">
+                    <button class="btn btn-primary d-block w-100 mt-3" type="submit" name="submit">Register</button>
+                  </div>
+                </form>
+                <div class="position-relative mt-5">
+                  <hr class="bg-300" />
+                  <div class="divider-content-center">or register with</div>
+                </div>
+                <div class="row g-2 mt-2">
+                  <div class="col-sm-6"><a class="btn btn-outline-google-plus btn-sm d-block w-100" href="#"><span class="fab fa-google-plus-g me-2" data-fa-transform="grow-8"></span> google</a></div>
+                  <div class="col-sm-6"><a class="btn btn-outline-facebook btn-sm d-block w-100" href="#"><span class="fab fa-facebook-square me-2" data-fa-transform="grow-8"></span> facebook</a></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+    <!-- ===============================================-->
+    <!--    End of Main Content-->
+    <!-- ===============================================-->
+
+
+    <!-- ===============================================-->
+    <!--    JavaScripts-->
+    <!-- ===============================================-->
+    <script src="{{ asset('falcon/public/vendors/popper/popper.min.js') }}"></script>
+    <script src="{{ asset('falcon/public/vendors/bootstrap/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('falcon/public/vendors/anchorjs/anchor.min.js') }}"></script>
+    <script src="{{ asset('falcon/public/vendors/is/is.min.js') }}"></script>
+    <script src="{{ asset('falcon/public/vendors/chart/chart.min.js') }}"></script>
+    <script src="{{ asset('falcon/public/vendors/leaflet/leaflet.js') }}"></script>
+    <script src="{{ asset('falcon/public/vendors/leaflet.markercluster/leaflet.markercluster.js') }}"></script>
+    <script src="{{ asset('falcon/public/vendors/leaflet.tilelayer.colorfilter/leaflet-tilelayer-colorfilter.min.js') }}"></script>
+    <script src="{{ asset('falcon/public/vendors/countup/countUp.umd.js') }}"></script>
+    <script src="{{ asset('falcon/public/vendors/fullcalendar/main.min.js') }}"></script>
+    <script src="{{ asset('falcon/public/assets/js/flatpickr.js') }}"></script>
+    <script src="{{ asset('falcon/public/vendors/dayjs/dayjs.min.js') }}"></script>
+    <script src="{{ asset('falcon/public/vendors/fontawesome/all.min.js') }}"></script>
+    <script src="{{ asset('falcon/public/vendors/lodash/lodash.min.js') }}"></script>
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
+    <script src="{{ asset('falcon/public/vendors/list.js/list.min.js') }}"></script>
+    <script src="{{ asset('falcon/public/assets/js/theme.js') }}"></script>
+  </body>
+
+</html>
